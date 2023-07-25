8fab8efef17aa2929222ce72cbdf5f4a09e4a7be
mythilyarasu <mythilyarasu@gmail.com>
2023-07-25 10:32:54 +0530

    update

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class  SignIn extends React.Component {
     }
     onSubmitSignIn = ()       =>
     {
-       fetch('http://localhost:3001/signin',{
+       fetch('https://smartbrainbackend-viuj.onrender.com/signin',{
         method: 'post',
         headers:{'Content-Type':'application/json'},
         body:JSON.stringify({

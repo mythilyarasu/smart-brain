70237810b03f018344bdbfdb9a88b532f92e8900
mythilyarasu <mythilyarasu@gmail.com>
2023-08-07 11:24:38 +0530

    update

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends React.Component{
             this.setState({password:event.target.value})
         }
         onSubmitSignIn = ()=> {
-       fetch('https://smartbrainbackend-viuj.onrender.com/register',{
+       fetch("https://smartbrainbackend-viuj.onrender.com/register",{
         method: 'post',
         headers:{'Content-Type':'application/json'},
         body:JSON.stringify({
